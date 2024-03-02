03e71d6c98a0f2f3153fff5b6559bb9a65aa83d0
TahirRida <tahirrida52@gmail.com>
2024-03-02 12:22:41 +0100

    authentication using JWT

--- Backend/src/main/java/br/com/fms/authentication/AuthenticationApplication.java
+++ Backend/src/main/java/br/com/fms/authentication/AuthenticationApplication.java
@@ -1,13 +1,13 @@
-package com.Backend.Backend;
+package br.com.fms.authentication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BackendApplication {
+public class AuthenticationApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
+		SpringApplication.run(AuthenticationApplication.class, args);
 	}
 
 }
